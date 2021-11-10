0cb8d43ea5c471dcba8949daa8becc4bf65dd5fa
«EmeryBV <>
2021-11-10 12:31:57 +0100

    Changes by sayamatha

--- Crypto-compression_of_3D_objects/Code/Crypto-compression_of_3D_objects/Edge.py
+++ Crypto-compression_of_3D_objects/Code/Crypto-compression_of_3D_objects/Edge.py
@@ -1,2 +1,6 @@
 class Edge:
-    print()
+
+    def __init__(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+
--- Crypto-compression_of_3D_objects/Code/Crypto-compression_of_3D_objects/Parser.py
+++ Crypto-compression_of_3D_objects/Code/Crypto-compression_of_3D_objects/Parser.py
@@ -1,9 +1,39 @@
-import numpy
-import open3d
+import math
+import numpy as np
+import open3d as o3d
 
-class Parser:
-    def __init__(self):
 
-        return
+def getNeighbors( vertex, faces ):
+    neighbors = set()
+    for l in faces:
+        if vertex in l:
+            neighbors.union( { v for v in l if v != vertex } )
+    return neighbors
 
-    def getMesh(self, git stat):
+
+def sortNeighbors( vertex, vertices, neighbors ):
+    upVector = np.array( [ 0., 1., 0. ] )
+    vertexPos = vertices[ vertex ]
+
+    angles = {}
+
+    for n in neighbors:
+        nPos = vertices[ n ]
+        normalizedVec = np.linalg.norm( nPos.substract( vertexPos ) )
+        angles[ n ] = math.acos( np.dot( upVector, normalizedVec ) ) * 180. /  np.pi
+
+    sortedNeighbors = { k : v for k, v in  sorted( angles.items(), key = lambda item : item[1],  reverse = True )}
+    return sortedNeighbors.keys()
+
+
+def readMesh(file):
+    mesh = o3d.io.read_triangle_mesh(file)
+
+    vertices  = np.asarray(mesh.vertices)
+    triangles = np.asarray(mesh.triangles)
+
+    neighbors = {}
+    for i in range ( 0 , len(vertices) + 1 ):
+        neighbors[ i ] = sortNeighbors( i, vertices, getNeighbors( i ) )
+
+    return vertices, triangles, neighbors
